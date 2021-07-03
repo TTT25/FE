96d91b12e6918677902ab1860dc9f7f83da191bc
TTT25 <67861923+TTT25@users.noreply.github.com>
2021-07-03 17:53:34 +0700

    npm install dhtmlx-gantt --save add thu vien de chay

--- src/Page/Task.js
+++ src/Page/Task.js
@@ -1,5 +1,5 @@
-import './Task.css';   
-import React,{Component} from 'react'; 
+import './Task.css';
+import React, { Component } from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import { TaskRows } from "../../src/Data";
@@ -7,128 +7,152 @@ import { Link } from "react-router-dom";
 import { useState } from 'react';
 // import AddIcon from '@material-ui/icons/Add';
 // import Controls from "../Page/Controls/Control"
-import {NewTask} from "./PopupNewTask/NewTask";
+import { NewTask } from "./PopupNewTask/NewTask";
 export default function DataTable() {
-const [data,setData] = useState(TaskRows);
-const handleDelete = (id) =>{
-  setData(data.filter(item=>item.id !== id));
-// const [openPopup, setOpenPopup] = useState(false);
-// const useStyles = makeStyles(theme => ({
-//     newButton:{
-//       position:'adsolute',
-//       right: '10px'
-//     }
+    const [data, setData] = useState(TaskRows);
+    const handleDelete = (id) => {
+        setData(data.filter(item => item.id !== id));
+        // const [openPopup, setOpenPopup] = useState(false);
+        // const useStyles = makeStyles(theme => ({
+        //     newButton:{
+        //       position:'adsolute',
+        //       right: '10px'
+        //     }
+    }
+
+
+    const columns = [
+        { field: "id", headerName: "ID", width: 130 },
+        { field: "groupname", headerName: "Groupname", width: 160 },
+        { field: "leader", headerName: "Leader", width: 160 },
+        { field: "taskname", headerName: "Taskname", width: 180 },
+        { field: "date", headerName: "Date", width: 120 },
+        { field: "term", headerName: "Term", width: 180 },
+        {
+            field: "request",
+            headerName: "Request",
+            width: 180,
+            renderCell: (params) => {
+                return ( <
+                    >
+                    <
+                    button className = "TaskListEdit" > Download < /button> <
+                    />
+                )
+            },
+        },
+        {
+            field: "complete",
+            headerName: "Complete",
+            width: 180,
+            renderCell: (params) => {
+                return ( <
+                    >
+                    <
+                    button className = "TaskListEdit" > Download < /button> <
+                    />
+                )
+            }
+        },
+        {
+            field: "action",
+            headerName: "Action",
+            width: 180,
+            renderCell: (params) => {
+                return ( <
+                    >
+                    <
+                    Link to = { "/Task/" + params.row.id } >
+                    <
+                    button className = "TaskListEdit" > Edit < /button> <
+                    /Link>
+
+                    <
+                    DeleteForeverIcon className = "TaskListDelete"
+                    onClick = {
+                        () => handleDelete(params.row.id) }
+                    /> <
+                    />
+                )
+            }
+
+        }
+    ];
+
+    const rows = [{
+            id: 1,
+            groupname: 'Nhóm 3',
+            leader: "Đô Lâm",
+            taskname: "Thiết kế cơ sở dữ liệu",
+            request: "Null",
+            date: "01/01/2021",
+            term: "01/02/2021",
+            complete: "null",
+        },
+        {
+            id: 2,
+            groupname: 'Nhóm 1',
+            leader: "Đô Lâm",
+            taskname: "Webside bán hàng",
+            date: "01/01/2021",
+            term: "01/02/2021",
+            complete: "null",
+        },
+        {
+            id: 3,
+            groupname: 'Nhóm 1',
+            leader: "Đô Lâm",
+            taskname: "Thiết kế cơ sở dữ liệu",
+            date: "01/01/2021",
+            term: "01/02/2021",
+            complete: "null",
+        },
+        {
+            id: 4,
+            groupname: 'Nhóm 1',
+            leader: "Đô Lâm",
+            taskname: "Thiết kế cơ sở dữ liệu",
+            date: "01/01/2021",
+            term: "01/02/2021",
+            complete: "null",
+        },
+    ];
+
+    return (
+
+        <
+        div className = "TaskListt"
+        style = {
+            { height: 400, width: '100%' } } >
+        <
+        DataGrid rows = { data }
+        disableSelectionOnClick columns = { columns }
+        pageSize = { 5 }
+        checkboxSelection / >
+
+        <
+        /div>
+
+    );
 }
 
+export class Task extends Component {
 
-const columns = [
-  { field: "id", headerName: "ID", width: 130 },
-  { field: "groupname", headerName: "Groupname", width: 160 },
-  { field: "leader", headerName: "Leader", width: 160 },
-  { field: "taskname", headerName: "Taskname", width: 180 },
-  { field: "date", headerName: "Date", width: 120 },
-  { field: "term",headerName: "Term", width: 180},
-  { field: "request", headerName: "Request", width: 180,
-        renderCell:(params)=>{
-        return(
-                <>
-                <button className="TaskListEdit">Download</button>
-                </>
-                )
-         },
-        },
-  { field: "complete", headerName: "Complete",width: 180, 
-      renderCell:(params)=>{
-    return(
-      <>
-      <button className="TaskListEdit">Download</button>
-      </>
-    )
-  }},
-  { field: "action",headerName: "Action",width: 180,
-        renderCell:(params)=>{
-          return(
-            <>
-            <Link to={"/Task/"+params.row.id}>
-            <button className="TaskListEdit">Edit</button>
-            </Link>
-            
-            <DeleteForeverIcon className="TaskListDelete" onClick={()=>handleDelete(params.row.id)}/>
-            </>
-          )
-        }
-
-      }
-  ];
-  
-  const rows = [
-    {
-      id: 1, 
-      groupname: 'Nhóm 3',
-      leader: "Đô Lâm",
-      taskname: "Thiết kế cơ sở dữ liệu",
-      request: "Null",
-      date:"01/01/2021",
-      term:"01/02/2021",
-      complete:"null",
-    },
-    {
-        id: 2, 
-        groupname: 'Nhóm 1',
-        leader: "Đô Lâm",
-        taskname: "Webside bán hàng",
-        date:"01/01/2021",
-        term:"01/02/2021",
-        complete:"null",  
-      },
-      {
-        id: 3, 
-        groupname: 'Nhóm 1',
-        leader: "Đô Lâm",
-        taskname: "Thiết kế cơ sở dữ liệu",
-        date:"01/01/2021",
-        term:"01/02/2021",
-        complete:"null",    
-      },
-      {
-        id: 4, 
-        groupname: 'Nhóm 1',
-        leader: "Đô Lâm",
-        taskname: "Thiết kế cơ sở dữ liệu",
-        date:"01/01/2021",
-        term:"01/02/2021",   
-        complete:"null", 
-      },
-  ];
-  
-  return (
-    
-    <div className="TaskListt" style={{ height: 400, width: '100%' }}>
-      <DataGrid 
-      rows={data} 
-      disableSelectionOnClick 
-      columns={columns} 
-      pageSize={5} checkboxSelection />
-      
-    </div>
-    
-  );
-  }
-
-export class Task extends Component{
- 
-    render(){
-        return(
-            <div classname="Task">
-                <DataTable/>
-                <Link to="/newtask"><button className="TaskAddButton">Create</button>
-                    </Link>
-                {/* <NewTask
-                 openPopup = {openPopup}
-                  setOpenPopup = {setOpenPopup}>
-        
-                </NewTask> */}
-            </div>
+    render() {
+        return ( <
+            div classname = "Task" >
+            <
+            DataTable / >
+            <
+            Link to = "/newtask" > < button className = "TaskAddButton" > Create < /button> <
+            /Link> {
+                /* <NewTask
+                                 openPopup = {openPopup}
+                                  setOpenPopup = {setOpenPopup}>
+                        
+                                </NewTask> */
+            } <
+            /div>
         )
     }
 }
